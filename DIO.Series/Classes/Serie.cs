0fa3fa49ec6c02f7fa6c48b3c05dba59b4f3e915
ribeiro-dev <romilson.2r@gmail.com>
2021-03-10 15:49:21 -0300

    First commit

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+
+        // Metodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = Id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return this.Id;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de início: " + this.Ano;
+            return retorno;
+        }
+
+    }
+}
